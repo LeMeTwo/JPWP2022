1b055897b06b535afa12a545a047d93187af970d
Ablesh1 <michal.ablewski19@gmail.com>
2022-05-17 02:33:31 +0200

    Change of details

--- addEvent.py
+++ addEvent.py
@@ -270,7 +270,7 @@ class Ui_Dialog(object):
         self.retranslateUi(Dialog)
         self.okcanButtons.accepted.connect(Dialog.accept)
         self.okcanButtons.rejected.connect(Dialog.reject)
-        self.ifRemindBox.toggled['bool'].connect(Dialog.exec)
+        self.ifRemindBox.toggled['bool'].connect(Dialog.execRemBox)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
 
     def retranslateUi(self, Dialog):
